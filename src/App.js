18c351dce9344deed9f3e4b1aa8376ce28fad82a
vattan2003 <VATTANsince2003@gmail.com>
2024-02-10 19:22:35 +0530

    updated version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Content from "./Components/Content";
 import Footer  from "./Components/Footer";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <>
      <Navbar />
+     <Hero />
      <Content />
      <Footer />
     </>
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,10 @@
+import banner from "./imgs/banner.png"
+
+function Hero(){
+    return(
+        <hero style={{}}>
+            <img src={banner} style={{width:"100%", height:"250px"}}></img>
+        </hero>
+    )
+}
+export default Hero
