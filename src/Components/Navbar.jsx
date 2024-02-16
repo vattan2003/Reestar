ddc8511ee43d393d1ac96cf5402b5c0903f3d01a
vattan2003 <VATTANsince2003@gmail.com>
2024-02-16 16:03:13 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,8 @@ function Navbar() {
         <nav className="bg-black text-white py-5 ">
             <center>
                 <h1 className="font-bold text-4xl font-serif">REESTAR</h1>
-                <a>Home | Recently Ordered | Contact</a>
+                <menu className="flex justify-evenly mt-3 border py-1 rounded"><i class="fa-brands fa-shopify"></i> <i class="fa-solid fa-truck-fast"></i><i class="fa-solid fa-location-dot"></i> <i class="fa-solid fa-bookmark"></i>  <i class="fa-solid fa-list"></i></menu>
+
             </center>
         </nav>
     )
